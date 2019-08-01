c9372480c2645082d6ee4274464c54ea53f629b4
Roberto De la Parra <r.delaparralombana@reply.de>
2019-08-01 09:13:40 +0200

    Improved type definition

--- src/reactblockui.d.ts
+++ src/reactblockui.d.ts
@@ -6,9 +6,9 @@ export type BlockUiProps<T = {}> = React.HTMLAttributes<HTMLElement> & {
     children?: any,
     renderChildren?: boolean, // default to true
     className?: string,
-    message?: any,
-    loader?: any,
-    tag?: any
+    message?: string | React.ReactNode,
+    loader?: ((...args: any[]) => any) | string | React.ReactNode,
+    tag?: (...args: any[]) => any | string,
 } & T;
 
 declare class BlockUi<T = { [key: string]: any }> extends React.Component<BlockUiProps<T>> { }
@@ -21,11 +21,11 @@ export type ReduxBlockUiProps<T = {}> = React.HTMLAttributes<HTMLElement> & {
     children?: any,
     renderChildren?: boolean, // default to true
     className?: string,
-    message?: any,
-    loader?: any,
-    tag?: any,
-    block: any,
-    unblock: any,
+    message?: string | React.ReactNode,
+    loader?: ((...args: any[]) => any) | string | React.ReactNode,
+    tag?: ((...args: any[]) => any) | string,
+    block: ((...args: any[]) => any) | RegExp | string | Array<any>,
+    unblock: ((...args: any[]) => any) | RegExp | string | Array<any>,
     onChange: (e: any) => void
 } & T;
 
